12b730f43c005acb6886e21a3a157e7e65244cbf
Георгий Бурмистров <georgiyburmistrov@MacBook-Pro-Georgij.local>
2024-08-03 13:51:50 +0400

    UserRole-enum added

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,4 +4,10 @@ namespace App\Enums;
 
 use Illuminate\Validation\Rules\Enum;
 
-final class UserRole extends Enum
+final class UserRole: int extends Enum { // или пример из кода Кузьмина?
+
+    const Admin = 1;
+    const Teacher = 2;
+    const Student = 3;
+
+}
